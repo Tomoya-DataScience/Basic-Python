dd4d6ab8710f9910e1dea42a3629d292b54eb80f
Tomoya-DataScience <hiroike.tomoya@gmail.com>
2022-09-11 21:52:52 +0900

    Cのソート部分変更しました Dの条件判定変更しました

--- AOJ/02/02_C.py
+++ AOJ/02/02_C.py
@@ -1,14 +1,3 @@
 a, b, c = (int(x) for x in input().split())
-if (a < b):
-    if (a < c):  # a < c < bのとき
-        b, c = c, b
-    elif (c < a):  # c < a < bのとき
-        a, b, c = c, a, b
-else:  # b <= aのとき
-    if (a < c):  # b <= a < cのとき
-        a, b = b, a
-    elif (c < a):  # b <= c < aのとき
-        a, b, c = b, c, a
-    else:  # c < b <= aのとき
-        a, b, c = c, a, b
-print(f"{a} {b} {c}")
+answer = sorted([a, b, c])
+print(f"{answer[0]} {answer[1]} {answer[2]}")
--- AOJ/02/02_D.py
+++ AOJ/02/02_D.py
@@ -1,7 +1,5 @@
 W, H, x, y, r = (int(x) for x in input().split())
-if (x >= r and r <= y and r <= H - y):  # 円が左に寄っているときの境界
-    print("Yes")
-elif (x <= W - r and r <= y and y <= H - r):  # 円が右に寄っているときの境界
+if (r <= x and x <= W - r and r <= y and y <= H - r):
     print("Yes")
 else:
     print("No")
