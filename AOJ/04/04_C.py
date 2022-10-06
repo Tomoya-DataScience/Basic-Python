9a533056357c883f07ad0e51e70daa453e1cab41
Tomoya-DataScience <hiroike.tomoya@gmail.com>
2022-10-06 20:25:47 +0900

    AOJ04_C 2nd

--- AOJ/04/04_C.py
+++ AOJ/04/04_C.py
@@ -1,21 +1,27 @@
-def calc(a, op, b):
-    if op == "+":
-        print(a + b)
-    elif op == "-":
-        print(a - b)
-    elif op == "*":
-        print(a * b)
-    elif op == "/":
-        print(a // b)
+def calc(a_list, op_list, b_list):
+    for i in range(len(a_list)):
+        if op_list[i] == "+":
+            print(a_list[i] + b_list[i])
+        elif op_list[i] == "-":
+            print(a_list[i] - b_list[i])
+        elif op_list[i] == "*":
+            print(a_list[i] * b_list[i])
+        elif op_list[i] == "/":
+            print(a_list[i] // b_list[i])
 
+
+a_list = []
+b_list = []
+op_list = []
 
 while (1):
     list = input().split()
-    a = int(list[0])
-    op = list[1]
-    b = int(list[2])
-# while文の外で計算すると入力を複数受け取れる
-    if op == "?":
+    if list[1] == "?":
         break
+    else:
+        op_list.append(list[1])
+    a_list.append(int(list[0]))
+    b_list.append(int(list[2]))
+# while文の外で計算すると入力を複数受け取れる
 
-    calc(a, op, b)
+calc(a_list, op_list, b_list)
